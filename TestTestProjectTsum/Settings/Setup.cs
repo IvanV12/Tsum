8f9422988f29ec94a73f1b3917c86bff60c30906
IvanV12 <mimilordi.19995@gmail.com>
2022-01-24 20:35:57 +0300

    Первый проект с ноута

---
+++ TestTestProjectTsum/Settings/Setup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTestProjectTsum
+{
+    public class Setup
+    {
+        public Setup () { }
+
+
+    }
+}
---
+++ TestTestProjectTsum/Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using Xunit;
+
+namespace TestTestProjectTsum
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Debug.WriteLine("Step_1");
+
+            Debug.WriteLine("Step_2");
+
+        }
+    }
+}
